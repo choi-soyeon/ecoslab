ec835e05dd0ae47c5dc32506d1c91b0fd734e7d8
choi-soyeon <94320053+choi-soyeon@users.noreply.github.com>
2026-07-10 06:31:24 +0000

    Deploying to gh-pages from @ choi-soyeon/ecoslab@675d8da4e66af58836587e162df5350ede3ba792 🚀

--- assets/js/google-analytics-setup.js
+++ assets/js/google-analytics-setup.js
@@ -3,4 +3,7 @@ function gtag() {
   window.dataLayer.push(arguments);
 }
 gtag("js", new Date());
-gtag("config", "G-EMF0DSXFLK");
+const analyticsPageTitle = document.querySelector('meta[name="analytics-page-title"]')?.content;
+gtag("config", "G-EMF0DSXFLK", {
+  page_title: analyticsPageTitle || document.title,
+});
